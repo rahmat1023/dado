f8e94adcbc4c45aed27745cab59ad2f49a802b59
rahmat1023 <rahmat1023@gmail.com>
2017-11-01 20:51:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,52 +3,62 @@
 		<title> Daftar Download </title>
 	</head>
 	<body>
-		<ul>
-			
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCSTV2ZDhWclV6NE0">MDM2 1</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCMWlYcE5hc084RTQ">MDM2 2</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCRVZBZXl1TzFNU1k">NG 12</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCSWhmZ3A1UGRiQTQ">RM 373</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCZUQ4Z2pFd2Yxb28">MFF 1</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B4OxXeRkj_vCTWNQa016WWNxUms">MFF 2</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B9vs_oQoCvvLeklxM0EtVWNyQ2s">HS 29</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B9vs_oQoCvvLN1psT2VfOGZSZEk">HS 30</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B9vs_oQoCvvLZGVZaU80VzJiTTA">HS 31</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B9vs_oQoCvvLbGpjd3d1OUs0TVE">HS 32</a>
-			</li>
-			
-			<li>
-				<a href="https://docs.google.com/uc?id=0B_RNb27JTyQ0YlJqbVZZV2gwVFE">WYWS 13</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B_RNb27JTyQ0eGNYUE9vY01vQTg">WYWS 14</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B_RNb27JTyQ0THJXbDZic3NLSm8">WYWS 15</a>
-			</li>
-			<li>
-				<a href="https://docs.google.com/uc?id=0B_RNb27JTyQ0ZWtzVWhOYUVoeTA">WYWS 16</a>
-			</li>
-			
-		</ul>	
+		Temperature of Love
+https://docs.google.com/uc?id=0BxL1NUzHSWPEZGFNdWhlLWFveVk
+https://docs.google.com/uc?id=0BxL1NUzHSWPEWVA5NHkzU2dnMUk
+https://docs.google.com/uc?id=0BxL1NUzHSWPEUkUtc0twa09rR3M
+https://docs.google.com/uc?id=0BxL1NUzHSWPEV3V3NkF6ZjVlREk
+https://docs.google.com/uc?id=0BxL1NUzHSWPEcVowVWtSQ3VxMFU
+https://docs.google.com/uc?id=0BxL1NUzHSWPELUVuaEFsTHRyN3c
+https://docs.google.com/uc?id=0BxL1NUzHSWPEQ3dKMW04VkEzRVE
+https://docs.google.com/uc?id=0BxL1NUzHSWPER2hrWGZFcFcwQWM
+https://docs.google.com/uc?id=0BxL1NUzHSWPEX3J5RHoyb3BMQWc
+https://docs.google.com/uc?id=0BxL1NUzHSWPEWFBVUVB2aUFxZVk
+https://docs.google.com/uc?id=0BxL1NUzHSWPESGxUNkI4MzhGNzA
+https://docs.google.com/uc?id=0BxL1NUzHSWPEQkhPVlVPUUR0SWM
+https://docs.google.com/uc?id=0BxL1NUzHSWPEX1FVaWZoU1RidzQ
+https://docs.google.com/uc?id=0BxL1NUzHSWPEWWpLZ1hJWExxX1k
+https://docs.google.com/uc?id=0BxL1NUzHSWPEdEltUktvV01XOHM
+https://docs.google.com/uc?id=0BxL1NUzHSWPESXB3aWF3M3RBUW8
+https://docs.google.com/uc?id=0BxL1NUzHSWPEbUpGeE1MQjVENUU
+https://docs.google.com/uc?id=0BxL1NUzHSWPEOExOZ1JOMzA2aWM
+https://docs.google.com/uc?id=0BxL1NUzHSWPEZUZHcWRkV0MzVkk
+https://docs.google.com/uc?id=0BxL1NUzHSWPEcWZPZHBTN0RiZk0
+https://docs.google.com/uc?id=0BxL1NUzHSWPEaHFUblNKS2FMazg
+https://docs.google.com/uc?id=0BxL1NUzHSWPEclNKR0JLS0JOczA
+https://docs.google.com/uc?id=0BxL1NUzHSWPEdV9PMk1vU2RoSTg
+https://docs.google.com/uc?id=0BxL1NUzHSWPETlFaekRlRzBvYmc
+https://docs.google.com/uc?id=0BxL1NUzHSWPEbTVMQlJ0Y21DcTg
+https://docs.google.com/uc?id=0BxL1NUzHSWPEdjE2QUc3MDFxN28
+
+MK 03
+https://docs.google.com/uc?id=0B4OxXeRkj_vCczZERjhMTnJUTDg
+
+RS 9,11,12
+https://docs.google.com/uc?id=0B4OxXeRkj_vCMUkxdWZheEdhVDA
+https://docs.google.com/uc?id=0B4OxXeRkj_vCNmczYXZ4QjVPYjg
+https://docs.google.com/uc?id=0B4OxXeRkj_vCQTJsMVB0bTREbFE
+
+LEDT 53
+https://docs.google.com/uc?id=0B4OxXeRkj_vCTXpTRHdIUGRiY00
+
+FR 00-01
+https://docs.google.com/uc?id=0B4OxXeRkj_vCY0pFc0VlaTVSV1k
+https://docs.google.com/uc?id=0B4OxXeRkj_vCZ2ZYSU5LN0dZa0U
+
+LoTJ 288
+https://docs.google.com/uc?id=0B4OxXeRkj_vCX1pSbjFnejAzODQ
+
+HS 33-36
+https://docs.google.com/uc?id=0B9vs_oQoCvvLUEhLRm1qbHpOLXc
+https://docs.google.com/uc?id=0B9vs_oQoCvvLaThFem4wbDV5eXc
+https://docs.google.com/uc?id=0B9vs_oQoCvvLX0pGcUl0QmZqTGc
+https://docs.google.com/uc?id=0B9vs_oQoCvvLR2pLN3pzVi1BU1E
+
+WYWS 17-20
+https://docs.google.com/uc?id=0B_RNb27JTyQ0WHpHMnVCZTBwZkk
+https://docs.google.com/uc?id=0B_RNb27JTyQ0R0xab3FoRzl0Qmc
+https://docs.google.com/uc?id=0B_RNb27JTyQ0TzRtTFZUVjBZdE0
+https://docs.google.com/uc?id=0B_RNb27JTyQ0cUhBejhXZlJEWXc
 
 	</body>
